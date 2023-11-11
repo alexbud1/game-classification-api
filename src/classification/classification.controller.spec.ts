bfcc95819f50db42c1f127ebf6c2a9a44d0174db
Oleksii Budzinskyi <alexbud1@pc-192-168-1-109.p.lodz.pl>
2023-11-11 20:10:42 +0100

    basic unit tests for controller and dto added

--- src/classification/classification.controller.spec.ts
+++ src/classification/classification.controller.spec.ts
@@ -15,4 +15,18 @@ describe('ClassificationController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  // Test the classify method
+  describe('classify', () => {
+    // Test 1
+    it('should return string for valid request and 200 status code', () => {
+      const response = controller.classify({
+        description: 'Your game description goes here',
+      });
+
+      expect(response).toMatchObject({
+        category: expect.any(String), // Expects the category property to be a string
+      });
+    });
+  });
 });
---
+++ src/classification/dto/game_category_classification.dto.spec.ts
@@ -0,0 +1,43 @@
+import { GameCategoryClassificationDto } from './game_category_classification.dto';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { ValidationError } from 'class-validator';
+
+export function stringified(errors: ValidationError[]): string {
+  return JSON.stringify(errors);
+}
+
+describe('GameCategoryClassificationDto', () => {
+  // Test 1
+  it('should only accept strings for description', async () => {
+    const importInfo = { description: 4567 };
+    const ofImportDto = plainToInstance(
+      GameCategoryClassificationDto,
+      importInfo,
+    );
+    const errors = await validate(ofImportDto);
+    expect(errors.length).not.toBe(0);
+    expect(stringified(errors)).toContain(`description must be a string`);
+  });
+
+  // Test 2
+  it('should not allow an empty description', async () => {
+    const importInfo = { description: '' };
+    const ofImportDto = plainToInstance(
+      GameCategoryClassificationDto,
+      importInfo,
+    );
+    const errors = await validate(ofImportDto);
+    expect(errors.length).not.toBe(0);
+    expect(stringified(errors)).toContain(`description should not be empty`);
+  });
+
+  // Test3
+  it('should have a valid example', () => {
+    const dto = new GameCategoryClassificationDto();
+    dto.description = 'Your game description goes here';
+    // Access and check the example
+    expect(dto.description).toBeDefined();
+    expect(dto.description).toEqual(expect.any(String)); // Assuming it should be a string
+  });
+});
