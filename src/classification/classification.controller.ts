69f82086fba1b16105d40b9d995b7fe5f677a1ea
Oleksii Budzinskyi <alexbud1@pc-192-168-1-109.p.lodz.pl>
2023-11-11 18:49:23 +0100

    docs updated for 400 status code

--- src/classification/classification.controller.ts
+++ src/classification/classification.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { GameCategoryClassificationDto } from './dto/game_category_classification.dto';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags, ApiBadRequestResponse } from '@nestjs/swagger';
 
 @ApiTags('classification')
 @Controller('classification')
@@ -18,6 +18,40 @@ export class ClassificationController {
             category: {
               type: 'string',
               description: 'The category',
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'The description is not provided or is invalid',
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            message: [
+              'description must be a string',
+              'description should not be empty',
+            ],
+            error: 'Bad Request',
+            statusCode: 400,
+          },
+          properties: {
+            message: {
+              type: 'array',
+              description: 'The error messages',
+              items: {
+                type: 'string',
+              },
+            },
+            error: {
+              type: 'string',
+              description: 'The error name',
+            },
+            statusCode: {
+              type: 'number',
+              description: 'The status code',
             },
           },
         },
